34c2a115a842883a33f0dbae56efbd0fdd1d65f4
Abel-Huang <497720975@qq.com>
2017-04-25 20:17:27 +0800

    util

--- util/get_from_db.py
+++ util/get_from_db.py
@@ -1,5 +1,32 @@
 import pymysql
 
-#从数据库中获取相应的数据
+# 连接配置信息
+config = {
+    'host': '127.0.0.1',
+    'port': 3306,
+    'user': 'root',
+    'password': '',
+    'db': 'classdata',
+    'charset': 'utf8',
+    'cursorclass': pymysql.cursors.DictCursor,
+}
 
 
+def get_db():
+    # 创建连接
+    conn = pymysql.connect(**config)
+    cur = conn.cursor()
+    # 执行sql语句
+    try:
+        # 执行sql语句，进行查询
+        sql = 'SELECT * FROM result'
+        cur.execute(sql)
+        # 获取查询结果
+        result = cur.fetchall()
+        print(result)
+        print(type(result))
+    finally:
+        cur.close()
+        conn.close()
+
+get_db()
