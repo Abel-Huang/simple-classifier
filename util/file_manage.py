bf3e3ab14dc429d4ff5c267c39e12f7a4021b6bc
Abel-Huang <497720975@qq.com>
2017-04-26 14:08:35 +0800

    path manager

--- util/file_manage.py
+++ util/file_manage.py
@@ -1,4 +1,11 @@
+#用于简化文件操作, 根据参数直接生成文件路径
 
-#用于简化文件操作
-def generic_file():
-    
+# 获取分类模型相关的路径
+def generic_ml_filename(ml_method, feature_type):
+    filename='../data/model/'+feature_type+'/'+ml_method+'_model.m'
+    return filename
+
+# 获取特征值相关的路径
+def generic_fea_filename(feature_type):
+    filename='../data/temp/'+feature_type+'/'
+    return filename
