93daccfa662a871a3d9b8f30316576f89c34aa68
Abel-Huang <497720975@qq.com>
2017-05-03 22:41:30 +0800

    test

--- test/brisk.py
+++ test/brisk.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('../data/train_set/sky/sky(1).jpg')
+img = cv2.imread('../data/train_set/sky/sky (25).jpg')
 cv2.imshow('hehe',img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 brisk = cv2.BRISK_create()
@@ -8,6 +8,6 @@ kp, des = brisk.detectAndCompute(gray, None)
 img = cv2.drawKeypoints(gray, kp, (255, 0, 0))
 cv2.imshow('brisk', img)
 print(des)
-print(type(des))
-print(des.shape)
+# print(type(des))
+# print(des.shape)
 cv2.waitKey(0)
