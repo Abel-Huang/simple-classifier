c06010db987bcac16984841a3ec77b0bef192840
Abel-Huang <497720975@qq.com>
2017-05-03 22:47:15 +0800

    test

--- test/brisk.py
+++ test/brisk.py
@@ -1,6 +1,6 @@
 import cv2
 
-img = cv2.imread('../data/train_set/sky/sky (25).jpg')
+img = cv2.imread('../data/test_set/sky/sky (57).jpg')
 cv2.imshow('hehe',img)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 brisk = cv2.BRISK_create()
